6f7566a754204e9774a9df1738a47d02a8631a78
Justin Gent <justinbgent@gmail.com>
2020-03-14 16:03:18 -0600

    Add Camera2d class to project.

---
+++ CameraFollow/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CameraFollow
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D player;
+        Vector2 playerPosition;
+        float speed = 300f;
+
+        Texture2D box;
+        Vector2 boxPosition;
+
+        KeyboardState keyState;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            Content.RootDirectory = "Content";
+            IsFixedTimeStep = false;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            playerPosition = new Vector2(graphics.PreferredBackBufferWidth / 4, graphics.PreferredBackBufferHeight / 2);
+            boxPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player = Content.Load<Texture2D>("circle");
+            box = Content.Load<Texture2D>("box");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Keys.W))
+            {
+                playerPosition.Y -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (keyState.IsKeyDown(Keys.S))
+            {
+                playerPosition.Y += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (keyState.IsKeyDown(Keys.A))
+            {
+                playerPosition.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (keyState.IsKeyDown(Keys.D))
+            {
+                playerPosition.X += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(player, playerPosition, null, Color.White, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
+
+            spriteBatch.Draw(box, boxPosition, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
