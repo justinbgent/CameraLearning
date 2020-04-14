23fbe815dbb4390509b39f6a0934e39643c8a478
Justin Gent <justinbgent@gmail.com>
2020-04-13 18:58:43 -0600

    Add comments for clarity

--- CameraFollow/Program.cs
+++ CameraFollow/Program.cs
@@ -45,42 +45,52 @@ namespace CameraFollow
                 game.Run();
         }
 
+        // Code rotates a 2d array clockwise 90 degrees
         public static void Rotate(int[][] matrix)
         {
+            // The space complexity for this function is always O(1)
+            // No more than 10 integers are created and used at a time
             int adjust = matrix.Length - 1;
+            // adjusting will adjust as the loops run 
             int adjusting = adjust;
 
+            // Note the nested loop. Even though nested, it is not truly O(n^2)
+            // due to the conditionals, (one of which updates while ran), the time complexity
+            // is really O(n) as rotation of each element occurs only once.
             for (int i = 0; i < matrix.Length / 2; i++)
             {
                 for (int j = 0; j < adjusting; j++)
                 {
+                    // this if statement ensures that elements will be rotated only once
                     if (j == 0 && i > 0)
                     {
+                        // starts j at the correct element
                         j = i;
+                        // updates the loop's conditional
                         adjusting -= 1;
-                        // For safety
+                        // For safety but probably unecesary:
                         if (j >= adjusting) break;
                     }
 
-                    int firstIndice = j;                                        // 1
-                    int secondIndice = adjust - i;                              // 2 - 0
-                    int storeValue = matrix[i][j];                              // 2
-                                                                                // save value for next rotated position
-                    int nextElementValue = matrix[firstIndice][secondIndice];   // 6
-                                                                                // input value of rotated element
-                    matrix[firstIndice][secondIndice] = storeValue;             // 2
-
                     // rotate value 90 degrees
+                    int indice1 = j;
+                    int indice2 = adjust - i;
+                    int storeValue = matrix[i][j];
+                    int nextElementValue = matrix[indice1][indice2];
+                    matrix[indice1][indice2] = storeValue;
+
+                    // this int is 
                     int timesLeft = 3;
                     int oldFirst;
+                    // yes another loop, still O(n), realize we are working with a 2d array here.
                     while (timesLeft > 0)
                     {
-                        oldFirst = firstIndice;
-                        firstIndice = secondIndice;
-                        secondIndice = adjust - oldFirst;
+                        oldFirst = indice1;
+                        indice1 = indice2;
+                        indice2 = adjust - oldFirst;
                         storeValue = nextElementValue;
-                        nextElementValue = matrix[firstIndice][secondIndice];
-                        matrix[firstIndice][secondIndice] = storeValue;
+                        nextElementValue = matrix[indice1][indice2];
+                        matrix[indice1][indice2] = storeValue;
                         timesLeft--;
                     }
                 }
